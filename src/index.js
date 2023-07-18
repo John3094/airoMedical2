b85fdd9847e0aa33f3e333f2c89a88ba28166c92
Yevhenii Yakovenko <yakovenkob@gmail.com>
2023-07-18 19:59:22 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/airoMedical">
+    <BrowserRouter basename="/airoMedical2">
       <App />
     </BrowserRouter>
   </React.StrictMode>
